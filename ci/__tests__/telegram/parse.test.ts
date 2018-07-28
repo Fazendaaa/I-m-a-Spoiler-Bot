049d3a7e6a3e5765d9ed1dc1e1bb6133bccbe1a0
farm <lukas_karotta@hotmail.com>
2018-07-28 12:32:46 -0300

    Fixing Travis integration.

--- ci/__tests__/telegram/parse.test.ts
+++ ci/__tests__/telegram/parse.test.ts
@@ -4,15 +4,8 @@ import { join } from 'path';
 import { toInline } from '../../../src/lib/telegram/parse';
 import { readMock } from '../../readMocks';
 
-const telegarfi18n = require('telegraf-i18n');
-const i18n = new telegarfi18n({
-    defaultLanguage: 'en',
-    allowMissing: true,
-    directory: join(__dirname, '../../../others/locales')
-});
-
 describe('Testing toInline', () => readMock('telegram', 'parse').forEach(({ locale, mocks }) =>
     describe(locale, () => mocks.forEach(({ label, input, output }) => test(label, () =>
-        expect(toInline({ translate: i18n, ...input })).toEqual(output)
+        expect(toInline(input)).toEqual(output)
     )))
 ));
